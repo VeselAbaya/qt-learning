ef1b88efd97f3e9c3cc055dcf8e6a1428b57f2aa
VeselAbaya <Vandervise465@gmail.com>
2018-04-03 10:22:50 +0300

    INCOMPLETE

--- Calculator/mainwindow.cpp
+++ Calculator/mainwindow.cpp
@@ -1,14 +1,51 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent),
+                                         ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    connect(ui->pushButton_0, SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_1, SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_2, SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_3, SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_4, SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_5, SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_6, SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_7, SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_8, SIGNAL(clicked()), this, SLOT(digits_numbers()));
+    connect(ui->pushButton_9, SIGNAL(clicked()), this, SLOT(digits_numbers()));
+
+    connect(ui->pushButton_plus, SIGNAL(clicked(bool)), this, SLOT(operations()));
+    connect(ui->pushButton_minus, SIGNAL(clicked(bool)), this, SLOT(operations()));
+    connect(ui->pushButton_mod, SIGNAL(clicked(bool)), this, SLOT(operations()));
+    connect(ui->pushButton_div, SIGNAL(clicked(bool)), this, SLOT(operations()));
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::digits_numbers() {
+    QPushButton* button = (QPushButton*)sender();
+
+    ui->label_result->setText(ui->label_result->text() + button->text());
+}
+
+void MainWindow::operations() {
+
+}
+
+void MainWindow::on_pushButton_dot_clicked() {
+    if (!ui->label_result->text().contains('.')) {
+        ui->label_result->setText(ui->label_result->text() + '.');
+    }
+}
+
+void MainWindow::on_pushButton_sign_clicked() {
+    ui->label_result->setText(QString::number((ui->label_result->text().toDouble() * (-1)), 'g', 30));
+}
+
+void MainWindow::on_pushButton_AC_clicked() {
+    ui->label_result->setText("");
 }
--- Calculator/mainwindow.h
+++ Calculator/mainwindow.h
@@ -4,11 +4,10 @@
 #include <QMainWindow>
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
@@ -17,6 +16,15 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+
+private slots:
+    void digits_numbers();
+    void operations();
+
+    void on_pushButton_dot_clicked();
+    void on_pushButton_sign_clicked();
+    void on_pushButton_AC_clicked();
 };
 
 #endif // MAINWINDOW_H
