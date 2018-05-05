0970156dce59682cb0a0307e7c7ab19879aad84a
VeselAbaya <Vandervise465@gmail.com>
2018-05-05 19:39:46 +0300

    done

--- Painter/main.cpp
+++ Painter/main.cpp
@@ -3,19 +3,19 @@
 
 /*! \mainpage This is LULPainter
  * \section Functionality
- * \subsection Grayscale
+ * \subsection first1 Grayscale
  * To invert some area choose Grayscale tool and clicked once on start point, then just released it on finish.
  * To grayscale whole image - Image -> Grayscale (ctrl+G)
  *
- * \subsection Invert color
+ * \subsection second2 Invert color
  * To invert some area choose Invert color tool and clicked once on start point, then just released it on finish.
  * To invert whole image - Image -> Invert colot (ctrl+I)
  *
- * \subsection Crop
+ * \subsection third3 Crop
  * To crop image choose Crop tool.
  * In opened dialog choose direction of crop and vertical and horizontal size (in pixels)
  *
- * \subsection Expanse
+ * \subsection fourth4 Expanse
  * To expanse image choose Expanse tool.
  * In opened dialog choose direction of expansion, backgorund-fill color, vertical and horizontal size (in pixels) */
 
